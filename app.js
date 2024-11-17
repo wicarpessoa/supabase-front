e80c3f392cc6c828e0862cb1f7659621eddeb5e6
Wicarpessoa <wicarpp@gmail.com>
2024-11-17 17:36:04 -0300

    feat: add modal

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const updateProductForm = document.querySelector('#update-product-form');
 const updateProductId = document.querySelector('#update-id');
 const updateProductName = document.querySelector('#update-name');
 const updateProductPrice = document.querySelector('#update-price');
+const updateProductDescription = document.querySelector('#update-description');
+const modal = document.querySelector('#update-modal');
+const closeModalButton = modal.querySelector('.close');
 
-// Function to fetch all products from the server
 async function fetchProducts() {
   const response = await fetch('http://localhost:3000/products');
   const products = await response.json();
-
+  console.log(products)
   // Clear product list
   productList.innerHTML = '';
 
@@ -30,11 +32,7 @@ async function fetchProducts() {
     // Add update button for each product
     const updateButton = document.createElement('button');
     updateButton.innerHTML = 'Update';
-    updateButton.addEventListener('click', () => {
-      updateProductId.value = product.id;
-      updateProductName.value = product.name;
-      updateProductPrice.value = product.price;
-    });
+    addUpdateButtonEventListener(updateButton, product);
     li.appendChild(updateButton);
 
     productList.appendChild(li);
@@ -42,39 +40,99 @@ async function fetchProducts() {
 }
 
 
-// Event listener for Add Product form submit button
+
 addProductForm.addEventListener('submit', async event => {
   event.preventDefault();
   const name = addProductForm.elements['name'].value;
   const price = addProductForm.elements['price'].value;
-  await addProduct(name, price);
+  const description = addProductForm.elements['description'].value;
+  await addProduct(name, price, description);
   addProductForm.reset();
   await fetchProducts();
 });
 
-// Function to add a new product
-async function addProduct(name, price) {
+
+async function addProduct(name, price, description) {
   const response = await fetch('http://localhost:3000/products', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ name, price })
+    body: JSON.stringify({ name, price, description })
   });
   return response.json();
 }
 
-// Function to delete a new product
+async function updateProduct(id, name, price, description) {
+  const response = await fetch(`http://localhost:3000/products/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name, price, description })
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    console.error('Erro ao atualizar produto:', error);
+  }
+
+  return response.json();
+}
+
 async function deleteProduct(id) {
   const response = await fetch('http://localhost:3000/products/' + id, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json'
     },
-    //body: JSON.stringify({id})
+
   });
   return response.json();
 }
 
-// Fetch all products on page load
+function openUpdateModal(product) {
+  modal.style.display = 'block';
+  updateProductId.value = product.id;
+  updateProductName.value = product.name;
+  updateProductPrice.value = product.price;
+  updateProductDescription.value = product.description
+}
+
+closeModalButton.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+function closeModal() {
+  modal.style.display = 'none';
+  updateProductForm.reset();
+}
+
+updateProductForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const id = updateProductId.value;
+  const name = updateProductName.value;
+  const price = updateProductPrice.value;
+  const description = updateProductDescription.value
+  await updateProduct(id, name, price, description);
+
+  closeModal()
+  await fetchProducts();
+});
+
+
+function addUpdateButtonEventListener(button, product) {
+  button.addEventListener('click', () => {
+    openUpdateModal(product);
+  });
+}
+
+
 fetchProducts();
